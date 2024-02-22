822a5672bcae900f5e26274c4c2a9b74bc43c715
Pramod Kumar Rout <pramodkumarrout243@gmail.com>
2024-02-22 13:29:20 +0530

    API Fetch update correctly

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const weatherIcon = document.querySelector(".weather-icon");
 
 
 async function checkWeather(city) {
-  const response = await fetch(apiUrl + city + '&appid=${apiKey}');
+  const response = await fetch(`${apiUrl}${city}&appid=${apiKey}`);
 
   if(response.status == 404) {
     document.querySelector(".error").style.display = "block";
